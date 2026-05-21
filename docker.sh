fcaa14066d828dca959b44fcc99711c8ccd38050
Anil <thapaanil62@gmail.com>
2026-05-21 21:37:58 +0545

    CI-CD Deployment Fixes

--- docker.sh
+++ docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sudo apt update 
-sudo apt install -y docker.io docker-compose
+sudo apt update
 
-sudo usermod -aG docker $USER
-sudo service docker start
+# Install Docker + Compose V2
+sudo apt install -y docker.io docker-compose-v2
+
+# Start Docker
+sudo systemctl start docker
 sudo systemctl enable docker
+
+# Add ubuntu user to docker group
+sudo usermod -aG docker ubuntu
+
+# Fix docker socket permission immediately
+sudo chmod 666 /var/run/docker.sock
